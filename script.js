53d137067638e6b4eb9311014859b115e1a84c70
mtaraq5@gmail.com <mtaraq@Mos-MacBook-Pro.local>
2022-03-14 22:27:12 -0400

    updates

--- script.js
+++ script.js
@@ -45,12 +45,12 @@ const num = [
   "2",
   "2",
   "2",
-  "8",
-  "9",
-  "10",
-  "jack",
-  "queen",
-  "king",
+  // "8",
+  // "9",
+  // "10",
+  // "jack",
+  // "queen",
+  // "king",
 ];
 
 /* Function to build the deck */
@@ -116,26 +116,6 @@ function winnerIs(card1, card2) {
   value1 = getValue(value1);
   value2 = getValue(value2);
 
-  // //give values to the letters
-  // if (value1 == "ace") {
-  //   value1 = 1;
-  // } else if (value1 == "jack") {
-  //   value1 = 11;
-  // } else if (value1 == "queen") {
-  //   value1 = 12;
-  // } else if (value1 == "king") {
-  //   value1 = 13;
-  // }
-  // if (value2 == "ace") {
-  //   value2 = 1;
-  // } else if (value2 == "jack") {
-  //   value2 = 11;
-  // } else if (value2 == "queen") {
-  //   value2 = 12;
-  // } else if (value2 == "king") {
-  //   value2 = 13;
-  // }
-
   //find the greater value between card1 and card2
   let winnerIs = Math.max(value1, value2);
   //return the greater card
@@ -156,87 +136,93 @@ function placeCardBottom(deck, card1, card2) {
 function draw(event) {
   event.preventDefault();
 
-  if (player1Deck.length !== 52 || player2Deck.length !== 52) {
-    let tempArr1 = [];
-    let tempArr2 = [];
+  let tempArr1 = [];
+  let tempArr2 = [];
 
-    //Dealing the cards for both players
-    let player1Card = dealCard(player1Deck);
-    let player2Card = dealCard(player2Deck);
+  //Dealing the cards for both players
+  let player1Card = dealCard(player1Deck);
+  let player2Card = dealCard(player2Deck);
 
-    // displaying the new deck count
-    player1Facedown.textContent = `Score: ${player1Deck.length}`;
-    player2Facedown.textContent = `Score: ${player2Deck.length}`;
+  // displaying the new deck count
+  player1Facedown.textContent = `Deck Count: ${player1Deck.length}`;
+  player2Facedown.textContent = `Deck Count: ${player2Deck.length}`;
 
-    //displaying the dealt card face up
-    player1Faceup.setAttribute("src", player1Card.img);
-    player2Faceup.setAttribute("src", player2Card.img);
+  //displaying the dealt card face up
+  player1Faceup.setAttribute("src", player1Card.img);
+  player2Faceup.setAttribute("src", player2Card.img);
 
-    /* Find the winner*/
+  /* Find the winner*/
 
-    while (player1Card.number === player2Card.number) {
-      tempArr1.push(player2Card);
+  // while the player1 card and player 2 cards are the same run these codes
+  while (player1Card.number === player2Card.number) {
+    tempArr1.push(player1Card);
+    //checking if the length of the deck can push out more 3 or 2 or 1 card
+    if (player1Deck.length > 3) {
       tempArr1.push(dealCard(player1Deck));
       tempArr1.push(dealCard(player1Deck));
       tempArr1.push(dealCard(player1Deck));
-
-      console.log(tempArr1);
-      // console.log(`the temp arr1: ${tempArr1[0].number}`);
-      // console.log(`the temp arr1: ${tempArr1[1].number}`);
-      // console.log(`the temp arr1: ${tempArr1[2].number}`);
-      // console.log(`the temp arr1: ${tempArr1[3].number}`);
-
-      player1Card = dealCard(player1Deck);
-
-      player1Faceup.setAttribute("src", player1Card.img);
-
-      console.log(`card being delt: ${player1Card.number}`);
-
-      player1Facedown.textContent = `Score: ${player1Deck.length}`;
-      player1Faceup.textContent = player1Card.number;
-
-      tempArr2.push(player2Card);
-      tempArr2.push(dealCard(player2Deck));
-      tempArr2.push(dealCard(player2Deck));
-      tempArr2.push(dealCard(player2Deck));
-
-      console.log(tempArr2);
-
-      player2Card = dealCard(player2Deck);
-      player2Faceup.setAttribute("src", player2Card.img);
-      console.log(`card being delt: ${player2Card.number}`);
-      player2Facedown.textContent = `Score: ${player2Deck.length}`;
-      player2Faceup.textContent = player2Card.number;
+    } else if (player1Deck.length > 2) {
+      tempArr1.push(dealCard(player1Deck));
+      tempArr1.push(dealCard(player1Deck));
+    } else if (player1Deck.length > 1) {
+      tempArr1.push(dealCard(player1Deck));
     }
-    console.log(`${tempArr1}`);
-    console.log(`${tempArr2}`);
-    const winner = winnerIs(player1Card, player2Card);
-    console.log(winner);
+    console.log(tempArr1);
+
+    player1Card = dealCard(player1Deck);
+
+    player1Faceup.setAttribute("src", player1Card.img);
+
+    console.log(`card being delt: ${player1Card.number}`);
+
+    player1Facedown.textContent = `Deck Count: ${player1Deck.length}`;
+    player1Faceup.textContent = player1Card.number;
+
+    tempArr2.push(player2Card);
+    //checking if the length of the deck can push out more 3 or 2 or 1 card
+    if (player2Deck.length > 3) {
+      tempArr2.push(dealCard(player2Deck));
+      tempArr2.push(dealCard(player2Deck));
+      tempArr2.push(dealCard(player2Deck));
+    } else if (player2Deck.length > 2) {
+      tempArr2.push(dealCard(player2Deck));
+      tempArr2.push(dealCard(player2Deck));
+    } else if (player2Deck.length > 1) {
+      tempArr2.push(dealCard(player2Deck));
+    }
+
+    console.log(tempArr2);
+
+    player2Card = dealCard(player2Deck);
+    player2Faceup.setAttribute("src", player2Card.img);
+    console.log(`card being delt: ${player2Card.number}`);
+    player2Facedown.textContent = `Deck Count: ${player2Deck.length}`;
+    player2Faceup.textContent = player2Card.number;
+  }
+  console.log(`${tempArr1}`);
+  console.log(`${tempArr2}`);
+  const winner = winnerIs(player1Card, player2Card);
+  console.log(winner);
+  console.log(`deck1 : ${player1Deck.length}`);
+  console.log(`deck2 : ${player2Deck.length}`);
+
+  if (winner.number == player1Card.number) {
+    console.log(tempArr1);
+    player1Deck = tempArr1.concat(tempArr2, player1Deck);
+
+    placeCardBottom(player1Deck, player1Card, player2Card);
     console.log(`deck1 : ${player1Deck.length}`);
     console.log(`deck2 : ${player2Deck.length}`);
-
-    if (winner.number == player1Card.number) {
-      console.log(tempArr1);
-      player1Deck = tempArr1.concat(tempArr2, player1Deck);
-
-      placeCardBottom(player1Deck, player1Card, player2Card);
-      console.log(`deck1 : ${player1Deck.length}`);
-      console.log(`deck2 : ${player2Deck.length}`);
-      player1Facedown.textContent = `Score: ${player1Deck.length}`;
-      winLose.textContent = ` ${input.value.toUpperCase()} WON THE ROUND`;
-    } else {
-      console.log(tempArr2);
-      player2Deck = tempArr2.concat(tempArr1, player2Deck);
-      placeCardBottom(player2Deck, player1Card, player2Card);
-      console.log(`deck1 : ${player1Deck.length}`);
-      console.log(`deck2 : ${player2Deck.length}`);
-      player2Facedown.textContent = `Score: ${player2Deck.length}`;
-      winLose.textContent = "Player 2 WON THE ROUND";
-    }
-  } else if (player1Deck.length === 52) {
-    winLose.textContent = ` ${input.value.toUpperCase()} WINS THE GAME`;
-  } else if (player2Deck.length === 52) {
-    winLose.textContent = "Player 2 WINS THE GAME!";
+    player1Facedown.textContent = `Deck Count: ${player1Deck.length}`;
+    winLose.textContent = ` ${input.value.toUpperCase()} WON THE ROUND`;
+  } else {
+    console.log(tempArr2);
+    player2Deck = tempArr2.concat(tempArr1, player2Deck);
+    placeCardBottom(player2Deck, player1Card, player2Card);
+    console.log(`deck1 : ${player1Deck.length}`);
+    console.log(`deck2 : ${player2Deck.length}`);
+    player2Facedown.textContent = `Deck Count: ${player2Deck.length}`;
+    winLose.textContent = "Player 2 WON THE ROUND";
   }
 }
 
@@ -252,8 +238,8 @@ function restart(event) {
   let player1Deck = deck.slice(0, half);
 
   let player2Deck = deck.slice(-half);
-  player1Facedown.textContent = `Score: ${player1Deck.length}`;
-  player2Facedown.textContent = `Score: ${player2Deck.length}`;
+  player1Facedown.textContent = `Deck Count: ${player1Deck.length}`;
+  player2Facedown.textContent = `Deck Count: ${player2Deck.length}`;
 
   player1Faceup.setAttribute("src", "img/52cards/red_joker.png");
   player2Faceup.setAttribute("src", "img/52cards/red_joker.png");
@@ -280,11 +266,22 @@ const half = Math.ceil(deck.length / 2);
 let player1Deck = deck.slice(0, half);
 
 let player2Deck = deck.slice(-half);
-player1Facedown.textContent = `Score: ${player1Deck.length}`;
-player2Facedown.textContent = `Score: ${player2Deck.length}`;
+player1Facedown.textContent = `Deck Count: ${player1Deck.length}`;
+player2Facedown.textContent = `Deck Count: ${player2Deck.length}`;
 
 /* Calling the functions */
 
+//Start the game
 start.addEventListener("click", startGame);
-player1Btn.addEventListener("click", draw);
+
+//main btn to play the game
+if (player1Deck.length !== 52 && player2Deck.length !== 52) {
+  player1Btn.addEventListener("click", draw);
+} else if (player1Deck.length === 52) {
+  console.log(`winner 1`);
+  winLose.textContent = ` ${input.value.toUpperCase()} WINS THE GAME`;
+} else if (player2Deck.length === 52) {
+  console.log(`winner 2`);
+  winLose.textContent = "Player 2 WINS THE GAME!";
+}
 restartBtn.addEventListener("click", restart);
